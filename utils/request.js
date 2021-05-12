2e2457da87bb8e828191f525fa55f0829f113d79
王冰炜 <619445114@qq.com>
2021-05-12 18:19:36 +0800

    chore: add front interceptor to detect token information

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import cookie from 'js-cookie'
+
 // 创建axios实例
 const service = axios.create({
   baseURL: 'http://localhost:9001', // api的base_url
   timeout: 20000 // 请求超时时间
 })
 export default service
+
+// http request 拦截器
+service.interceptors.request.use(
+  config => {
+    // 判断cookie中是否有名称为guli_token数据
+    if (cookie.get('guli_token')) {
+      config.headers['token'] = cookie.get('guli_token');
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err);
+  })
